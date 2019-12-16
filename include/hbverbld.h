a65742c96ba694a9900944480e4aefdf8d5df190
hernad <hernad@bring.out.ba>
2019-12-16 09:00:28 +0100

    harupdf headers

---
+++ include/hbverbld.h
@@ -0,0 +1,17 @@
+/*
+ * Version information and build time switches.
+ *
+ * Copyright 2008-present Przemyslaw Czerpak <druzus / at / priv.onet.pl>
+ *
+ * This file is generated automatically by Harbour preprocessor
+ * and is covered by the same license as Harbour PP
+ */
+
+#define HB_VER_ORIGIN_URL        "https://github.com/hernad/my-harbour/"
+#define HB_VER_COMMIT_ID         "ad9f9cc2afb24f4c5fc2d12583744f966450ad0d"
+#define HB_VER_COMMIT_ID_SHORT   "ad9f9cc"
+#define HB_VER_COMMIT_YEAR       "2019"
+#define HB_VER_COMMIT_REV        1912160738
+#define HB_VER_COMMIT_INFO       "2019-12-16 08:38:44 +0100"
+#define HB_PLATFORM              "linux"
+#define HB_COMPILER              "gcc"
