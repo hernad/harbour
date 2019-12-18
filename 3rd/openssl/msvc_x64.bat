42683050e9b2329b45ee4b95dc036a1eff0d13cc
Ernad Husremovic <hernad@bring.out.ba>
2019-12-18 08:40:47 +0100

    NOTES openssl, setup openssl win64 build - msvc_x64.bat

---
+++ 3rd/openssl/msvc_x64.bat
@@ -0,0 +1,29 @@
+
+set PATH=c:\windows;c:\windows\system32
+set PATH=%PATH%;C:\Program Files\Git\cmd
+set PATH=%PATH%;C:\Users\hernad\AppData\Local\Programs\Microsoft VS Code\bin
+set WINSDK_VER=10.0.18362.0
+
+
+set HB_INSTALL_PREFIX=c:\users\hernad\harbour-hernad\harbour
+
+REM amd64 ili x86
+call "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat" amd64
+
+set PATH=%PATH%;C:\Program Files (x86)\Windows Kits\10\bin\%WINSDK_VER%\x64
+
+
+
+SET PATH=%PATH%;C:\Strawberry\c\bin
+SET PATH=%PATH%;C:\Strawberry\perl\bin
+SET PATH=%PATH%;C:\Strawberry\perl\site\bin
+
+SET PATH=%PATH%;C:\users\hernad\NASM64
+
+
+cd \users\hernad\harbour\3rd\openssl
+
+echo this location should be root location of git repository
+echo Setup configure script:
+echo perl Configure VC-WIN64A
+echo nmake -f makefile
