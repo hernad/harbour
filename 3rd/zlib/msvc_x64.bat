4809d4430587b3044238d281dab9992ff4c85167
Ernad Husremovic <hernad@bring.out.ba>
2019-12-18 16:52:52 +0100

    msvc_x64 zlib

--- 3rd/zlib/msvc_x64.bat
+++ 3rd/zlib/msvc_x64.bat
@@ -2,8 +2,8 @@ set PATH=c:\windows;c:\windows\system32
 set PATH=%PATH%;C:\Program Files\Git\cmd
 set PATH=%PATH%;C:\Users\hernad\AppData\Local\Programs\Microsoft VS Code\bin
 set WINSDK_VER=10.0.18362.0
-set POSTGRES_DIR=postgresql-12.1
-set POSTGRESQL_TARGET=c:\users\hernad\x64\libpq
+set ZLIB_DIR=zlib-1.2.11
+set POSTGRESQL_TARGET=c:\users\hernad\x64\zlib
 
 set HB_INSTALL_PREFIX=c:\users\hernad\harbour-hernad\harbour
 
@@ -21,11 +21,19 @@ SET PATH=%PATH%;C:\Strawberry\perl\site\bin
 SET PATH=%PATH%;C:\users\hernad\NASM64
 
 
-cd \users\hernad\harbour\3rd\postgresql\%POSTGRES_DIR%
+cd \users\hernad\harbour\3rd\zlib\%ZLIB_DIR%
 
 echo this location should be root location of git repository
 echo Setup configure script:
 
-perl src\tools\msvc\build.pl
+nmake -f win32/Makefile.msc
 
-perl src\tools\msvc\install.pl %POSTGRESQL_TARGET% client
+mkdir c:\users\hernad\x64\zlib
+mkdir c:\users\hernad\x64\zlib\include
+mkdir c:\users\hernad\x64\zlib\lib
+
+copy zlib.lib c:\users\hernad\x64\zlib\lib
+copy *.h c:\users\hernad\x64\zlib\include
+
+cd \users\hernad\harbour\3rd\zlib
+
