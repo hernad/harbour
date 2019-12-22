813a27bbcd5bdd02fcf1f0586aafd3785fe3a945
Ernad Husremovic <hernad@bring.out.ba>
2019-12-22 15:13:34 +0100

    hbmk2 new zlib

--- include/hbverbld.h
+++ include/hbverbld.h
@@ -8,10 +8,10 @@
  */
 
 #define HB_VER_ORIGIN_URL        "git@github.com:hernad/harbour/"
-#define HB_VER_COMMIT_ID         "fb521d507a0d98ccbe8f44fb9dd54527a3af7d48"
-#define HB_VER_COMMIT_ID_SHORT   "fb521d50"
+#define HB_VER_COMMIT_ID         "55f64fa9055b62c0fb3a95602dd2ec5fe67f437c"
+#define HB_VER_COMMIT_ID_SHORT   "55f64fa9"
 #define HB_VER_COMMIT_YEAR       "2019"
-#define HB_VER_COMMIT_REV        1912221100
-#define HB_VER_COMMIT_INFO       "2019-12-22 12:00:03 +0100"
+#define HB_VER_COMMIT_REV        1912221338
+#define HB_VER_COMMIT_INFO       "2019-12-22 14:38:26 +0100"
 #define HB_PLATFORM              "win"
 #define HB_COMPILER              "msvc64"
