387710e0c0577d8d4203a6bdde9abbceb939df5d
hernad <hernad@bring.out.ba>
2019-12-24 14:12:01 +0100

    libxml2 configure

--- 3rd/libxml2/gcc_x64.sh
+++ 3rd/libxml2/gcc_x64.sh
@@ -9,10 +9,18 @@ PREFIX=$ROOT_3RD
 cd $LIB_SRC
 
 echo "===  lib_src: $LIB_SRC ==== prefix: === $PREFIX ====================="
-sh ./autogen.sh --prefix=$PREFIX --with-iconv=$ROOT_3RD/include --with-zlib=$ROOT_3RD/include
-sh  ./configure --without-python 
-#make clean
-make
-make install DESTDIR=$PREFIX
+#sh ./autogen.sh
+#--build=$PREFIX \
+   
+
+sh  ./configure \
+   --without-python \
+   --prefix=$PREFIX \
+   --with-iconv=$ROOT_3RD/include \
+   --with-zlib=$ROOT_3RD/include
+
+ 
+make install
+
 cd ..
 
