c92e587e3d1acab9affaf2fc8361e22364068e0d
hernad <hernad@bring.out.ba>
2019-12-25 19:25:08 +0100

    uuid

---
+++ 3rd/uuid/gcc_x64.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+source ../../make_envars.sh
+
+LIB_NAME=uuid
+LIB_SRC="uuid-1.6.2"
+PREFIX=$ROOT_3RD/$LIB_NAME
+
+cd $LIB_SRC
+
+echo "========== $PREFIX ====================="
+./configure --prefix=$PREFIX
+make clean
+make
+make install
+cd ..
+
