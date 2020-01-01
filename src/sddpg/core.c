dc569f08a871b238434f0d827cb802a69a05d314
hernad <hernad@bring.out.ba>
2020-01-01 10:50:14 +0100

    note about my work

--- src/sddpg/core.c
+++ src/sddpg/core.c
@@ -1,8 +1,11 @@
 /*
  * Postgre SQL Database Driver
  *
- * Copyright 2007 Mindaugas Kavaliauskas <dbtopas at dbtopas.lt>
- *
+ * Copyright 
+ * 
+ * - 2007 Mindaugas Kavaliauskas <dbtopas at dbtopas.lt>
+ * - 2014-2019 Ernad Husremovic <hernad@bring.out.ba>
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
