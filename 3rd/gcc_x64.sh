d71751d76f1d2805489312f3723a6aa48dfa6c86
Ernad Husremovic <hernad@bring.out.ba>
2024-09-30 17:48:20 +0200

    postgresql stop build ne kontam

--- 3rd/gcc_x64.sh
+++ 3rd/gcc_x64.sh
@@ -6,44 +6,46 @@ source ../make_envars.sh
 export ZLIB_BUILD=
 
 cd zlib
-source gcc_x64.sh
+    ./gcc_x64.sh
 cd ..
 
+
 cd uuid
-source gcc_x64.sh
+   ./gcc_x64.sh
 cd ..
 
 cd libiconv
-source gcc_x64.sh
+  ./gcc_x64.sh
 cd ..
 
 unset LIBXML2_BUILD
 cd libxml2
-source gcc_x64.sh
+  ./gcc_x64.sh
 cd ..
 
 unset LIBXSLT_BUILD
 cd libxslt
-source gcc_x64.sh
+   ./gcc_x64.sh
 cd ..
 
 unset OPENSSL_BUILD
 cd openssl
-source gcc_x64.sh
+  ./gcc_x64.sh
 cd ..
+
 
 unset POSTGRESQL_BUILD
 cd postgresql
-source gcc_x64.sh
+  ./gcc_x64.sh
 cd ..
 
 unset CURL_BUILD
 cd curl
-source gcc_x64.sh
+ ./gcc_x64.sh
 cd ..
 
 unset SQLITE3_BUILD
 cd sqlite3
-source gcc_x64.sh
+ ./gcc_x64.sh
 cd ..
 
