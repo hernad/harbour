6268014e96f43ba15d931ac90fa8928c8bb65cb4
Ernad Husremovic <hernad@bring.out.ba>
2024-09-30 16:29:06 +0200

    ACDIR

--- 3rd/libxml2/gcc_x64.sh
+++ 3rd/libxml2/gcc_x64.sh
@@ -6,6 +6,8 @@ LIB_NAME=libxml2
 LIB_SRC=libxml2
 PREFIX=$ROOT_3RD/libxml2
 
+echo ACDIR=$(pwd)
+
 cd $LIB_SRC
 
 echo "===  lib_src: $LIB_SRC ==== prefix: === $PREFIX ====================="
@@ -18,11 +20,13 @@ echo "===  zlib: $ROOT_3RD/zlib/include ==="
    
 rm -f aclocal.m4 
 rm -rf m4
-aclocal --system-acdir=../$(pwd) && libtoolize --force && automake --add-missing && autoreconf -f
+
+aclocal -f --system-acdir=$ACDIR && libtoolize --force && autoreconf -f
 automake --add-missing
 
+#--without-python \
+
 bash  ./configure \
-   --without-python \
    --prefix=$PREFIX \
    --with-iconv=$ROOT_3RD/libiconv/include \
    --with-zlib=$ROOT_3RD/zlib/include
