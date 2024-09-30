dce837123039b04d4029a2068c620b391a678e45
Ernad Husremovic <hernad@bring.out.ba>
2024-09-30 16:45:14 +0200

    M4DIR

--- 3rd/libxml2/gcc_x64.sh
+++ 3rd/libxml2/gcc_x64.sh
@@ -6,7 +6,7 @@ LIB_NAME=libxml2
 LIB_SRC=libxml2
 PREFIX=$ROOT_3RD/libxml2
 
-echo ACDIR=$(pwd)
+echo M4DIR=$(pwd)/m4
 
 cd $LIB_SRC
 
@@ -21,7 +21,7 @@ echo "===  zlib: $ROOT_3RD/zlib/include ==="
 rm -f aclocal.m4 
 #rm -rf m4
 mkdir -p m4
-cp -av ../m4/* m4/ 
+cp -av $M4DIR/* m4/ 
 aclocal -I $ACDIR --force && libtoolize --force && autoreconf -f
 automake --add-missing
 
