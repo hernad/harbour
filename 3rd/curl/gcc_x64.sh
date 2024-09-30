9658d92ba35a972c28276cdc3c44ab9a5310c616
Ernad Husremovic <hernad@bring.out.ba>
2024-09-30 15:32:31 +0200

    7z

--- 3rd/curl/gcc_x64.sh
+++ 3rd/curl/gcc_x64.sh
@@ -3,6 +3,13 @@
 set -o errexit
 source ../../make_envars.sh
 
+SEVENZ_EXE=7z
+
+if [[ -f $ROOT_DIR/tools/rhel/x64/7z.sh ]] ; then
+   SEVENZ_EXE=$ROOT_DIR/tools/rhel/x64/7z.sh
+fi
+
+
 LIB_NAME=curl
 LIB_SRC=curl-7.67.0
 PREFIX=$ROOT_3RD/$LIB_NAME
@@ -11,7 +18,8 @@ echo "===  lib_src: $LIB_SRC ==== prefix: === $PREFIX ====================="
 
 [ -n "$ROOT_DIR" ] && rm -rf $ROOT_DIR/3rd/$LIB_NAME/$LIB_SRC
 
-$ROOT_DIR/tools/rhel/x64/7z.sh x $LIB_SRC.7z
+
+$SEVENZ_EXE x $LIB_SRC.7z
 
 cd $LIB_SRC
 
